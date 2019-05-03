b16a31ed2276bb0bbfb1469b0941847ac94bc37b
Saeid Asadi <saeidasadi1995@gmail.com>
2019-05-03 11:51:50 +0430

    Consume contex in child components Comment: Context defaultValue is still useless

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Parent from './Context/Parent';
+import Main from './Context2/Main';
 
 export default class App extends React.Component {
     render() {
         return (
-            <Parent/>
+            <Main/>
         )
     }
 }
