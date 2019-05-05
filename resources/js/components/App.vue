c1b5c97856463cfb2219d28762f8672bc4575488
saeid <saeid@gmail.com>
2019-05-05 17:38:12 +0430

    move router items to its own layout

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,28 +1,18 @@
 <template>
     <div>
-        <h1>Hello App! dasdasd Hel</h1>
+        <RouterLayout></RouterLayout>
 
-        <p>
-            <router-link to="/foo">Go to Foo</router-link>
-            <router-link to="/bar">Go to Bar</router-link>
-            <router-link to="/user/100">Go to User 100</router-link>
-            <router-link to="/user/200">Go to User 200</router-link>
-            <router-link to="/user/300-">Go to User 300-</router-link>
-            <router-link to="/user/400-saeid">Go to User 400-saeid</router-link>
-            <router-link to="/user/400-saeid/profile">Go to User 400-saeid > Profile</router-link>
-            <router-link to="/user/400-saeid/posts">Go to User 400-saeid > Posts</router-link>
-            <router-link :to="{name:'f'}">Foo With Named Route</router-link>
-            <router-link to="/example">Go To Example</router-link>
-            <router-link to="/example-2">Go To Example 2</router-link>
-        </p>
-
-        <router-view></router-view>
     </div>
 </template>
 
 <script>
+    import RouterLayout from './Layouts/RouterLayout';
 
     export default {
         name: "App",
+
+        components: {
+            RouterLayout,
+        }
     }
 </script>
---
+++ resources/js/components/Layouts/RouterLayout.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Hello App! dasdasd Hel</h1>
+
+        <p>
+            <router-link to="/foo">Go to Foo</router-link>
+            <router-link to="/bar">Go to Bar</router-link>
+            <router-link to="/user/100">Go to User 100</router-link>
+            <router-link to="/user/200">Go to User 200</router-link>
+            <router-link to="/user/300-">Go to User 300-</router-link>
+            <router-link to="/user/400-saeid">Go to User 400-saeid</router-link>
+            <router-link to="/user/400-saeid/profile">Go to User 400-saeid > Profile</router-link>
+            <router-link to="/user/400-saeid/posts">Go to User 400-saeid > Posts</router-link>
+            <router-link :to="{name:'f'}">Foo With Named Route</router-link>
+            <router-link to="/example">Go To Example</router-link>
+            <router-link to="/example-2">Go To Example 2</router-link>
+        </p>
+
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "RouterLayout",
+    }
+</script>
