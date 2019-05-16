dba96ac77a365eea7de814e98d394e98143298e2
Saeid Asadi <saeidasadi1995@gmail.com>
2019-05-16 20:54:07 +0430

    change http loading manner

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,11 +4,10 @@ import Vue from 'vue';
 import store from './store';
 import router from './router';
 import ElementUI from 'element-ui';
-import Http from './extensions/http';
 import App from './components/App';
+import './extensions/http';
 
 Vue.use(ElementUI);
-Vue.use(Http);
 
 const app = new Vue({
     el: '#app',
