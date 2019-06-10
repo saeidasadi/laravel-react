e351a14e2758b7b4ad4f0a0fce61454b5d3248e8
saeid <saeid@gmail.com>
2019-06-10 11:27:50 +0430

    break routes into multiple files

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,65 +1,14 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import coreRoutes from './routes/core';
 
 Vue.use(VueRouter);
 
-const Foo = {template: '<div>foo</div>'};
-
-const Bar = {template: '<div>bar</div>'};
-
-const Unknown = {template: '<div>404 - You Missed Out</div>'};
-
-const User = {
-    template: '<div>Hello User {{ $route.params.id }}, Slug: {{ $route.params.slug }}, HASH: {{ $route.hash }} -  Subview: <router-view></router-view></div>',
-    watch: {
-        '$route'(to, from) {
-            console.log(to, from);
-        }
-    }
-};
-
-const UserProfile = {
-    template: '<div>User Profile {{ $route.params.id }} Ok</div>',
-};
-const UserPosts = {
-    template: '<div>User Posts</div>',
-};
-const UserHome = {
-    template: '<div>User Home</div>',
-};
-
-// Using group put two components in same file, if we dont use it, there will be 2 chunk files
-const ExampleComponent = () => import(/* webpackChunkName: "group-example" */ './components/ExampleComponent');
-const ExampleLazy = () => import(/* webpackChunkName: "group-example" */ './components/ExampleLazy');
-
-const routes = [
-    {name: "f", path: '/foo', component: Foo},
-    {path: '/bar', component: Bar},
-    {
-        path: '/user/:id-:slug',
-        component: User,
-        children: [
-            {
-                path: '',
-                component: UserHome
-            },
-            {
-                path: 'profile',
-                component: UserProfile
-            },
-            {
-                path: 'posts',
-                component: UserPosts
-            }
-        ],
-    },
-    {path: '/example-2', component: ExampleLazy},
-    {path: '/example', component: ExampleComponent},
-    {path: '*', component: Unknown}
-];
+let routes = []
+    .concat(coreRoutes);
 
 const router = new VueRouter({
-    routes
+    routes,
 });
 
 export default router;
